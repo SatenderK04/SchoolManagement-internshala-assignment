809c8c97829e32d45616795286046ec55607afc3
SatenderK <satender12304@gmail.com>
2025-03-08 00:18:47 +0530

    .

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use("/api/schools", schoolRoutes);
+app.use("/api", schoolRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on PORT ${PORT}`);

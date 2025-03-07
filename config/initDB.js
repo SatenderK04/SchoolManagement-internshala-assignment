a38c12ed35b19dcab88ab9b5f187e685aa974428
SatenderK <satender12304@gmail.com>
2025-03-07 19:13:02 +0530

    DATABASE server intialized

--- config/initDB.js
+++ config/initDB.js
@@ -4,10 +4,10 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const db = mysql.createPool({
-  host: process.env.DB_HOST || "localhost",
-  user: process.env.DB_USER || "root",
-  password: process.env.DB_PASSWORD || "Hello@2004",
-  database: process.env.DB_NAME || "schoolmanagement",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   connectionLimit: 10,
 });
 
